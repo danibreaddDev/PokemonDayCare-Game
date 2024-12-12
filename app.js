11dba6eb9b48a840e14b0ba9902fe3c2ce54ad2f
Daniel Pamblanco <danielpamblancomestre@gmail.com>
2024-12-12 19:23:15 +0100

    cambios

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let container_app = document.getElementById("app");
+let container_start = document.getElementById("start");
+let button_go = document.getElementById("go");
 container_app.style.display = "none";
+button_go.addEventListener("click", () => {
+    container_start.style.display = "none";
+    container_app.style.display = "block";
+
+
+});
