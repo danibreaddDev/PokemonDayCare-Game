89756c6afc9247df8aaf6ace744e2d72ecca8690
Dani <danielpamblancomestre@gmail.com>
2024-12-17 00:17:38 +0100

    cambios

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//app
 function addPokemon(myPokemon) {
   let name = myPokemon.getAttribute("pokemon");
   let team = JSON.parse(localStorage.getItem("pokemons"));
@@ -36,9 +37,13 @@ let container_start = document.getElementById("start");
 let button_go = document.getElementById("go");
 let button_reset = document.getElementById("reset");
 let pokemons_pc = document.querySelectorAll(".pc-pokemons div");
+let audio = document.querySelectorAll("audio");
+audio[0].src = "/audio/temon.mp3";
 let pokemonMenuContainer = document.getElementById("choosePokes");
 container_app.style.display = "none";
 button_go.addEventListener("click", () => {
+  console.log(audio);
+  audio[0].src = "audio/theme.mp3";
   setStaticPokemon();
   container_start.style.display = "none";
   container_app.style.display = "block";
@@ -46,15 +51,13 @@ button_go.addEventListener("click", () => {
 button_reset.addEventListener("click", () => {
   localStorage.clear();
   for (let index = 0; index < pokemons_pc.length; index++) {
-    pokemons_pc[index].style  = "";
-    
+    pokemons_pc[index].style = "";
   }
 });
 
 for (let index = 0; index < pokemons_pc.length; index++) {
   pokemons_pc[index].addEventListener("click", () => {
     addPokemon(pokemons_pc[index]);
-   
   });
 }
 
@@ -68,3 +71,46 @@ if (localStorage.getItem("pokemons") !== null) {
     });
   }
 }
+//game
+class Task {
+  #description;
+  #completed;
+  constructor(description, completed) {
+    this.#description = description;
+    this.#completed = completed;
+  }
+  set description(description) {
+    this.#description = description;
+  }
+  get description() {
+    return this.#description;
+  }
+  set completed(completed) {
+    this.#completed = completed;
+  }
+  get completed() {
+    return this.#completed;
+  }
+}
+
+function addTasks() {
+  let team = JSON.parse(localStorage.getItem("pokemons"));
+  let arr_tasks = [
+    new Task(`Acaricia a ${team[0]}`, false),
+    new Task(`Acaricia a ${team[1]}`, false),
+    new Task("Acaricia a Golduck", false),
+    new Task("Acaricia a dragonair", false),
+    new Task("Busca pokemon entre la hierba alta", false),
+  ];
+  let tasks = document.getElementsByClassName("tasks")[0];
+  console.log(tasks);
+  console.log(arr_tasks);
+
+  for (let index = 0; index < arr_tasks.length; index++) {
+    let task_div = document.createElement("div");
+    task_div.className = "task";
+    task_div.innerHTML = `<p>${arr_tasks[index].description}</p>`;
+    tasks.appendChild(task_div);
+  }
+}
+addTasks();
