6a5331d5f6657d370c1d823d34b771226c9cf03a
Daniel Pamblanco <danielpamblancomestre@gmail.com>
2025-01-15 18:36:36 +0100

    cambios

--- app.js
+++ app.js
@@ -84,6 +84,24 @@ if (localStorage.getItem("pokemons") !== null) {
     });
   }
 }
+//cambiar modo
+let btn_toggle = document.getElementById("toggle");
+let root = document.querySelector(":root");
+console.log(btn_toggle);
+btn_toggle.addEventListener("click", () => {
+  root.classList.toggle("dark");
+  if (root.className == "dark") {
+    btn_toggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M0 7h2v2H0zm4 4H2V9h2zm4 2v-2H4v2H2v2h2v-2zm8 0H8v2H6v2h2v-2h8v2h2v-2h-2zm4-2h-4v2h4v2h2v-2h-2zm2-2v2h-2V9zm0 0V7h2v2z"/></svg>`;
+    audio[0].src = "/audio/temondark.mp3";
+  } else {
+    btn_toggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8 6h8v2H8zm-4 4V8h4v2zm-2 2v-2h2v2zm0 2v-2H0v2zm2 2H2v-2h2zm4 2H4v-2h4zm8 0v2H8v-2zm4-2v2h-4v-2zm2-2v2h-2v-2zm0-2h2v2h-2zm-2-2h2v2h-2zm0 0V8h-4v2zm-10 1h4v4h-4z"/></svg>`;
+
+  }
+
+});
+console.log(root.className);
+
+
 //game
 class Task {
   id;
