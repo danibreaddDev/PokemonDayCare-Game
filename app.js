3b98e21b38b5af5d56bfbf1990648f369bc3e187
Dani <danielpamblancomestre@gmail.com>
2025-01-19 17:38:47 +0100

    cambios

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ let button_go = document.getElementById("go");
 let button_reset = document.getElementById("reset");
 let pokemons_pc = document.querySelectorAll(".pc-pokemons div");
 let audio = document.querySelectorAll("audio");
+let video = document.querySelector("video");
 let container_loading = document.getElementById("loading");
 let btn_reload = "";
 audio[0].src = "/audio/temon.mp3";
@@ -38,6 +39,8 @@ let pokemons = Array.from(document.querySelectorAll(".pokemon-static"));
 let dialog = document.querySelector("dialog");
 container_app.style.display = "none";
 button_go.addEventListener("click", () => {
+  video.muted;
+  video.style.display = "none";
   if (localStorage.getItem("pokemons") == null) {
     dialog.innerHTML = ` <div><h3>Selecciona Pokemon</h3>
         <button class="btn_app" id="cerrar">Cerrar</button></div>`;
@@ -66,7 +69,6 @@ button_reset.addEventListener("click", () => {
   }
 });
 
-
 for (let index = 0; index < pokemons_pc.length; index++) {
   pokemons_pc[index].addEventListener("click", () => {
     addPokemon(pokemons_pc[index]);
@@ -90,16 +92,18 @@ console.log(btn_toggle);
 btn_toggle.addEventListener("click", () => {
   root.classList.toggle("dark");
   if (root.className == "dark") {
+    video.style.display = "flex";
+    video.play();
     btn_toggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M0 7h2v2H0zm4 4H2V9h2zm4 2v-2H4v2H2v2h2v-2zm8 0H8v2H6v2h2v-2h8v2h2v-2h-2zm4-2h-4v2h4v2h2v-2h-2zm2-2v2h-2V9zm0 0V7h2v2z"/></svg>`;
     audio[0].src = "/audio/temondark.mp3";
   } else {
+    video.style.display = "none";
+    video.muted;
     btn_toggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8 6h8v2H8zm-4 4V8h4v2zm-2 2v-2h2v2zm0 2v-2H0v2zm2 2H2v-2h2zm4 2H4v-2h4zm8 0v2H8v-2zm4-2v2h-4v-2zm2-2v2h-2v-2zm0-2h2v2h-2zm-2-2h2v2h-2zm0 0V8h-4v2zm-10 1h4v4h-4z"/></svg>`;
     audio[0].src = "/audio/temon.mp3";
   }
-
 });
 console.log(root.className);
-
 
 //game
 class Task {
